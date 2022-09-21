287f58ac4ef34ec1cd52d44432e5302016a3f8b2
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-09-21 14:38:42 -0700

    built header section

--- portfolio-2022/components/Header.tsx
+++ portfolio-2022/components/Header.tsx
@@ -1,15 +1,42 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {}
 
-function Header({ }: Props) {
+export default function Header({}: Props) {
     return (
-        <header>
-            <div>
+        <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl
+            mx-auto z-20 xl:items-center'>
+            <div className='flex flex-row items-center'>
             {/* {social icons} */}
+            <SocialIcon url="https://twitter.com/abdul48811" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://github.com/Abdulrahman48811" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://instagram.com/abdulrahmancodes" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://linkedin.com/in/abdulrahman-mohammed-b48456230/" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            </div>
+
+            <div className="flex flex-row items-center text-gray-300 cursor-pointer" >
+            <SocialIcon
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <p className='upppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
             </div>
         </header>
     )
 }
 
-export default Header
