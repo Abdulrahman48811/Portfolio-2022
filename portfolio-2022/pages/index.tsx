387f244c670840a7d156c43c9d55edbbee5e37b1
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-09-26 13:46:09 -0700

    building the work experience and cards section

--- portfolio-2022/pages/index.tsx
+++ portfolio-2022/pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import WorkExperience from '../components/WorkExperience'
 const Home: NextPage = () => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandotory 
@@ -18,13 +19,15 @@ const Home: NextPage = () => {
       <section id='hero' className='snap-start'>
         <Hero />
       </section>
-      
+
       <section id="about" className='snap-center'>
         <About />
       </section>
 
       {/* {experience} */}  
-      
+      <section id="experience" className='snap-center'>
+        <WorkExperience />
+      </section>
       {/* {skills} */}  
       
       {/* {projects} */}  
