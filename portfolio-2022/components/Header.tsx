e5ab0ddcd49d586d0e2efb8184818bfd9538e954
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-09-30 19:40:50 -0700

    added tailwind scrollbar

--- portfolio-2022/components/Header.tsx
+++ portfolio-2022/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from "framer-motion"
+import Link from 'next/link'
 type Props = {}
 
 export default function Header({ }: Props) {
@@ -42,6 +43,7 @@ export default function Header({ }: Props) {
                 />
             </motion.div>
 
+                <Link href="#contact">
             <motion.div
                 initial={{
                     x: 500,
@@ -65,6 +67,7 @@ export default function Header({ }: Props) {
                 />
                 <p className='upppercase hidden md:inline-flex text-sm text-gray-400'>Get in Touch</p>
             </motion.div>
+            </Link>
         </header>
     )
 }
