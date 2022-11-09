8415db78683f232b6d8af9fd120d993ceed4a1fe
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-08 21:00:50 -0800

    implemented skills animation

--- portfolio-2022/components/Skills.tsx
+++ portfolio-2022/components/Skills.tsx
@@ -21,11 +21,17 @@ export default function Skills({ skills }: Props) {
       </h3>
 
       <div className='grid grid-cols-4 gap-5'>
-        {skills?.map((skill) => (
-          <Skill 
+        {skills?.slice(0, skills.length / 2).map((skill) => (
+          <Skill
             key={skill._id} skill={skill}
           />
+        ))}
 
+        {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+          <Skill
+            key={skill._id} skill={skill}
+            directionLeft
+          />
         ))}
       </div>
     </motion.div>
